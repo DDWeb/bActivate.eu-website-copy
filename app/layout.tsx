f50b60b02bd521898bca532da5042d79aab41229
Martin Hinz <martin@tooshy.dk>
2026-01-27 08:09:55 +0100

    Add Google Tag Manager scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Fira_Sans_Condensed, Poppins } from "next/font/google"; // Import Fira Sans Condensed and Poppins
+import Script from "next/script";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -31,6 +32,21 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${firaSansCondensed.variable} ${poppins.variable}`}>
       <body className={poppins.className}>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WZLKN7T3');`}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WZLKN7T3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
         <Footer />
