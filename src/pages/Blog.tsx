03dcac3f72244a48b5fc59904031b8067cc465b1
Martin Hinz <martin@tooshy.dk>
2026-03-10 11:09:25 +0100

    Blog: center image, remove eyebrow text, overlap cards on hero

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -7,36 +7,31 @@ import { blogPosts } from '@/lib/blogData';
 export default function Blog() {
     return (
         <main>
-            <section className={styles.mainSection}>
+            <section className={styles.hero}>
                 <img
                     src="/images/foal-bg.jpg"
                     alt="Foal Background"
-                   
                     className={styles.backgroundImage}
-                    style={{ objectFit: 'cover' }}
-                   
+                    style={{ objectFit: 'cover', objectPosition: 'center' }}
                 />
                 <div className={styles.overlay}></div>
+                <div className={styles.heroContent}>
+                    <h1 className={styles.title}>Blog</h1>
+                </div>
+            </section>
 
-                <div className={styles.contentWrapper}>
-                    <div className={styles.headerContent}>
-                        <div className={styles.eyebrow}>bActivate Around The World</div>
-                        <h1 className={styles.title}>Blog</h1>
-                    </div>
-
-                    <div className={styles.container}>
-                        <hr className={styles.divider} />
-                        <div className={styles.grid}>
-                            {blogPosts.map((post, index) => (
-                                <article key={index} className={styles.card}>
-                                    <h3 className={styles.postTitle} dangerouslySetInnerHTML={{ __html: post.title }}></h3>
-                                    <div className={styles.date}>{post.date}</div>
-                                    <Link href={`/blog/${post.slug}`} className={styles.readMore}>
-                                        Read more <span className={styles.arrow}>&rarr;</span>
-                                    </Link>
-                                </article>
-                            ))}
-                        </div>
+            <section className={styles.blogSection}>
+                <div className={styles.container}>
+                    <div className={styles.grid}>
+                        {blogPosts.map((post, index) => (
+                            <article key={index} className={styles.card}>
+                                <h3 className={styles.postTitle} dangerouslySetInnerHTML={{ __html: post.title }}></h3>
+                                <div className={styles.date}>{post.date}</div>
+                                <Link to={`/blog/${post.slug}`} className={styles.readMore}>
+                                    Read more <span className={styles.arrow}>&rarr;</span>
+                                </Link>
+                            </article>
+                        ))}
                     </div>
                 </div>
             </section>
