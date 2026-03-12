72e0aae1b9c99cfc259c1bf166fc5d4759177e1a
Martin Hinz <martin@tooshy.dk>
2026-03-12 12:46:48 +0100

    Add Google Customer Reviews survey opt-in on order confirmation

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -32,6 +32,41 @@ export default function Shop() {
         };
 
         document.getElementById('my-store-130367502')?.appendChild(script);
+
+        // Google Customer Reviews - survey opt-in after order placement
+        // @ts-ignore
+        window.renderOptIn = function () {
+            // @ts-ignore
+            if (window.Ecwid) {
+                // @ts-ignore
+                window.Ecwid.OnOrderPlaced.add(function (order: any) {
+                    const deliveryDate = new Date();
+                    deliveryDate.setDate(deliveryDate.getDate() + 7);
+                    const estimatedDelivery = deliveryDate.toISOString().split('T')[0];
+
+                    // @ts-ignore
+                    window.gapi.load('surveyoptin', function () {
+                        // @ts-ignore
+                        window.gapi.surveyoptin.render({
+                            "merchant_id": 5744487013,
+                            "order_id": String(order.orderNumber),
+                            "email": order.email,
+                            "delivery_country": order.shippingPerson.countryCode,
+                            "estimated_delivery_date": estimatedDelivery
+                        });
+                    });
+                });
+            }
+        };
+
+        if (!document.getElementById('google-customer-reviews-script')) {
+            const googleScript = document.createElement('script');
+            googleScript.id = 'google-customer-reviews-script';
+            googleScript.src = 'https://apis.google.com/js/platform.js?onload=renderOptIn';
+            googleScript.async = true;
+            googleScript.defer = true;
+            document.head.appendChild(googleScript);
+        }
     }, []);
 
     return (
